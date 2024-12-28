2183c1052070cacd8cb98ce0359eea64fb75e8a4
uzziahlukeka <uzziahlukeka@gmail.com>
2024-12-28 17:42:27 +0100

    first commit and note design

---
+++ app/Models/Note.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Note extends Model
+{
+    /** @use HasFactory<\Database\Factories\NoteFactory> */
+    use HasFactory , HasUuids ;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class );
+    }
+
+}
